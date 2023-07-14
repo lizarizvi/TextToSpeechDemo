607da1b0e653580ad81135229df7236c61639275
LIZA RIZVI <lizarizvi333@gmail.com>
2023-07-14 17:23:23 +0530

    setting viewBinding and Toast

--- app/src/main/java/com/example/texttospeech/MainActivity.kt
+++ app/src/main/java/com/example/texttospeech/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.texttospeech
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.widget.Toast
+import com.example.texttospeech.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+
+    private var tts : TextToSpeech? = null
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        tts = TextToSpeech(this, this)
+        binding?.speakBtn?.setOnClickListener {
+            Toast.makeText(this,"Enter the text to speak",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onInit(p0: Int) {
+
     }
 }
